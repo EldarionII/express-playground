823275f0694d31aac9c13187b1542b5a241ad9d4
Fedor Kostrov <kostrov.fedor@gmail.com>
2019-01-01 19:58:29 +0300

    express-mongodb-CRUD

--- server.js
+++ server.js
@@ -1,17 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
+const mongodbUrl = 'mongodb://admin:admin123@ds026658.mlab.com:26658/express-playground';
+
+let db;
+
+MongoClient.connect(mongodbUrl, (error, client) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    db = client.db('express-playground');
+
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
 });
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    db.collection('quotes').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        // renders index.ejs
+        res.render('index.ejs', {quotes: result})
+    })
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    db.collection('quotes').save(req.body, (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log('saved to database');
+
+        res.redirect('/')
+    })
+});
+
+app.put('/quotes', (req, res) => {
+    db.collection('quotes')
+        .findOneAndUpdate({name: 'Yoda'}, {
+            $set: {
+                name: req.body.name,
+                quote: req.body.quote
+            }
+        }, {
+            sort: {_id: -1},
+            upsert: true
+        }, (err, result) => {
+            if (err) return res.send(err);
+            res.send(result)
+        })
+});
+
+app.delete('/quotes', (req, res) => {
+    db.collection('quotes').findOneAndDelete({name: req.body.name},
+        (err, result) => {
+            if (err) return res.send(500, err);
+            res.send({message: 'A darth vadar quote got deleted'})
+        })
 });
