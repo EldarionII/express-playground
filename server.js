f3f07d5aec30df0fcd449a78860ee249886fedb3
Fedor Kostrov <kostrov.fedor@gmail.com>
2019-01-02 15:28:43 +0300

    connected front to back

--- server.js
+++ server.js
@@ -1,36 +1,52 @@
 const express = require('express');
+const fs = require('fs')
+const https = require('https')
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
+    , Server = require('mongodb').Server;
 
 const app = express();
 
-const mongodbUrl = 'mongodb://admin:admin123@ds026658.mlab.com:26658/express-playground';
-
 let db;
+let mongoClient = new MongoClient(new Server('localhost', 27017));
 
-MongoClient.connect(mongodbUrl, (error, client) => {
+mongoClient.connect((error, client) => {
     if (error) {
         return console.log(error)
     }
 
     db = client.db('express-playground');
 
-    app.listen(3000, () => {
+    https.createServer({
+        key: fs.readFileSync('server.key'),
+        cert: fs.readFileSync('server.cert')
+    }, app).listen(3000, () => {
         console.log('listening on 3000')
     })
 });
-
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
-app.get('/', (req, res) => {
-    db.collection('quotes').find().toArray((err, result) => {
+app.get(/^((?!api).)*$/, (req, res) => {
+    db.collection('users').find().toArray((err, result) => {
         if (err) return console.log(err);
         // renders index.ejs
-        res.render('index.ejs', {quotes: result})
+    });
+    res.render('index.ejs')
+});
+
+app.get('/api/users', (req, res) => {
+    db.collection('users').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        res.send(result)
     })
 });
 
@@ -66,6 +82,8 @@ app.delete('/quotes', (req, res) => {
     db.collection('quotes').findOneAndDelete({name: req.body.name},
         (err, result) => {
             if (err) return res.send(500, err);
-            res.send({message: 'A darth vadar quote got deleted'})
+            res.send({message: 'A darth vader quote got deleted'})
         })
 });
+
+require('./users/test').test()
